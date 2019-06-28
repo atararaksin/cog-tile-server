24fd70c2a7500635d21e26649361f9c47e552e9e
Andrey Tararaksin <atararaksin@gmail.com>
2019-06-28 13:17:06 +0300

    Use GDALRasterSource

--- build.sbt
+++ build.sbt
@@ -4,11 +4,19 @@ version := "0.1"
 
 scalaVersion := "2.12.8"
 
+resolvers ++= Seq(
+  "locationtech-releases" at "https://repo.locationtech.org/content/groups/releases",
+  "locationtech-snapshots" at "https://repo.locationtech.org/content/groups/snapshots",
+  "Azavea Public Builds" at "https://dl.bintray.com/azavea/geotrellis"
+)
+
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-http"   % "10.1.8",
   "com.typesafe.akka" %% "akka-stream" % "2.5.23",
   "com.typesafe.akka" %% "akka-actor" % "2.5.23",
   "ch.megard" %% "akka-http-cors" % "0.4.0",
-  "org.locationtech.geotrellis" %% "geotrellis-raster" % "2.3.1",
-  "org.locationtech.geotrellis" %% "geotrellis-spark" % "2.3.1"
+  "org.locationtech.geotrellis" %% "geotrellis-raster" % "3.0.0-M3",
+  "org.locationtech.geotrellis" %% "geotrellis-spark" % "3.0.0-M3",
+  "com.azavea.geotrellis" %% "geotrellis-contrib-vlm" % "3.14.0",
+  "com.azavea.geotrellis" %% "geotrellis-contrib-gdal" % "3.14.0"
 )
--- src/main/scala/CogService.scala
+++ src/main/scala/CogService.scala
@@ -1,14 +1,10 @@
 package cogtileserver
 
+import geotrellis.contrib.vlm.gdal.{GDALRasterSource, GDALWarpOptions}
 import geotrellis.proj4.WebMercator
-import geotrellis.raster.crop.Crop
-import geotrellis.raster.io.geotiff.{Auto, GeoTiff, MultibandGeoTiff}
-import geotrellis.raster.{MultibandTile, RasterExtent}
 import geotrellis.spark.SpatialKey
 import geotrellis.spark.tiling.ZoomedLayoutScheme
-import geotrellis.util.{FileRangeReader, StreamingByteReader}
-import geotrellis.vector.Extent
-import geotrellis.raster.reproject.Reproject
+import geotrellis.raster.resample.Bilinear
 
 object CogService {
 
@@ -16,29 +12,17 @@ object CogService {
     val layoutScheme = ZoomedLayoutScheme(WebMercator)
     val layout = layoutScheme.levelForZoom(zoom).layout
 
-    val geoTiff = CachingGeoTiffReader[MultibandTile].cacheRead(uri)
-
-    val keyExtent: Extent = layout.mapTransform(SpatialKey(x, y))
-    val reprojectedKeyExtent = keyExtent.reproject(layoutScheme.crs, geoTiff.crs)
-
-    def extractTile(extent: Extent, tiff: GeoTiff[MultibandTile]) = tiff
-      .getClosestOverview(layout.cellSize, Auto(0))
-      .crop(extent, Crop.Options(clamp = false))
-      .raster
-      .reproject(geoTiff.crs, layoutScheme.crs, Reproject.Options(targetCellSize = Some(layout.cellSize)))
-      .resample(RasterExtent(keyExtent, layoutScheme.tileSize, layoutScheme.tileSize))
-      .tile
-
-    geoTiff
-      .extent
-      .intersection(reprojectedKeyExtent)
-      .map(extent => extractTile(extent, geoTiff).renderPng().bytes)
+    GDALRasterSource(uri)
+      .reproject(WebMercator)
+      .tileToLayout(layout, Bilinear)
+      .read(SpatialKey(x, y))
+      .map(_.renderPng().bytes)
   }
 
   def getBounds(uri: String): String = {
 
-    val geotiff = MultibandGeoTiff.streaming(StreamingByteReader(FileRangeReader(uri)))
-    val extent = geotiff.extent.reproject(geotiff.crs, WebMercator)
+    val rasterSource = GDALRasterSource(uri)
+    val extent = rasterSource.extent.extent.reproject(rasterSource.crs, WebMercator)
 
     "{\"bounds\"" + s": [${extent.xmin}, ${extent.ymin}, ${extent.xmax}, ${extent.ymax}]}"
   }
